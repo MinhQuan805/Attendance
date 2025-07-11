985879d5c4c70e8ed39aefac37342bf29a9058b6
Minh Quan <minhquanvo2006@gmail.com>
2025-07-11 10:18:05 +0700

    first

--- routes/admin/attendance.route.js
+++ routes/admin/attendance.route.js
@@ -1,10 +1,5 @@
 const express = require("express");
-const multer = require('multer');
-const storageMulter = require('../../helpers/storageMulter');
 const router = express.Router();
-
-const uploadCloud = require('../../middleware/admin/uploadCloud.middleware');
-const upload = multer();
 
 const controller = require("../../controllers/admin/attendance.controller.js");
 
